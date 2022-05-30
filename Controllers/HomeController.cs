0424f98aaebc6ab1e34a0800a264c64c14d0844d
ianMizi <ianMizi09@gmail.com>
2022-05-30 14:26:00 +0800

    Your application description page.Testing GIT

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace ClientApplication.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.Testing GIT";
 
             return View();
         }
